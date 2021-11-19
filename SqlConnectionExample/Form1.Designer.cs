40bc40a8c2199053c3f9b4e8bf80e44c62a02e23
kerim.calik <kerim.calik@koton.com.tr>
2021-11-19 23:11:21 +0300

    Add project files.

---
+++ SqlConnectionExample/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SqlConnectionExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnPersonelAra = new System.Windows.Forms.Button();
+            this.txtPersonelId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPersonelSoyadi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPersonelAdi = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridPersonel = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPersonel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSil);
+            this.panel1.Controls.Add(this.btnGuncelle);
+            this.panel1.Controls.Add(this.btnKaydet);
+            this.panel1.Controls.Add(this.btnPersonelAra);
+            this.panel1.Controls.Add(this.txtPersonelId);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtPersonelSoyadi);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtPersonelAdi);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 193);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(286, 145);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 6;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(205, 145);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 6;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(124, 145);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 6;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnPersonelAra
+            // 
+            this.btnPersonelAra.Location = new System.Drawing.Point(249, 28);
+            this.btnPersonelAra.Name = "btnPersonelAra";
+            this.btnPersonelAra.Size = new System.Drawing.Size(75, 23);
+            this.btnPersonelAra.TabIndex = 5;
+            this.btnPersonelAra.Text = "Pesonel Ara";
+            this.btnPersonelAra.UseVisualStyleBackColor = true;
+            this.btnPersonelAra.Click += new System.EventHandler(this.btnPersonelAra_Click);
+            // 
+            // txtPersonelId
+            // 
+            this.txtPersonelId.Location = new System.Drawing.Point(124, 28);
+            this.txtPersonelId.Name = "txtPersonelId";
+            this.txtPersonelId.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelId.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Personel ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Personel Soyadı";
+            // 
+            // txtPersonelSoyadi
+            // 
+            this.txtPersonelSoyadi.Location = new System.Drawing.Point(124, 99);
+            this.txtPersonelSoyadi.Name = "txtPersonelSoyadi";
+            this.txtPersonelSoyadi.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelSoyadi.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Personel Adı";
+            // 
+            // txtPersonelAdi
+            // 
+            this.txtPersonelAdi.Location = new System.Drawing.Point(124, 63);
+            this.txtPersonelAdi.Name = "txtPersonelAdi";
+            this.txtPersonelAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtPersonelAdi.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridPersonel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 193);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 257);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridPersonel
+            // 
+            this.dataGridPersonel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridPersonel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridPersonel.Location = new System.Drawing.Point(0, 0);
+            this.dataGridPersonel.Name = "dataGridPersonel";
+            this.dataGridPersonel.Size = new System.Drawing.Size(800, 257);
+            this.dataGridPersonel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridPersonel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridPersonel;
+        private System.Windows.Forms.Button btnPersonelAra;
+        private System.Windows.Forms.TextBox txtPersonelId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPersonelSoyadi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPersonelAdi;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnKaydet;
+    }
+}
+
---
+++ SqlConnectionExample/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlConnectionExample
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection("Data Source=BABEGT;Initial Catalog=AdventureWorks2012;Integrated Security=True");
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+
+            connection.Open();
+
+            GetAllEmployee(string.Empty);
+        }
+
+        private void GetAllEmployee(string personelId)
+        {
+            var sql = "select * from Person.Person order by BusinessEntityID desc;";
+
+            if (!string.IsNullOrEmpty(personelId))
+            {
+                sql = "select * from Person.Person where BusinessEntityId = "  + personelId + " order by BusinessEntityID desc";
+            }
+
+            
+            SqlCommand sqlCommand = new SqlCommand(sql, connection);
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable table = new DataTable();
+
+            sqlDataAdapter.Fill(table);
+
+            dataGridPersonel.DataSource = table;
+        }
+
+        private void btnPersonelAra_Click(object sender, EventArgs e)
+        {
+            var personelId = txtPersonelId.Text;
+            GetAllEmployee(personelId);
+
+            //SqlCommand command = new SqlCommand("select FirstName, LastName from Person.Person where BusinessEntityID = " + personelId, connection);
+
+            //SqlDataReader reader = command.ExecuteReader();
+
+            //if (reader.Read())
+            //{
+            //    txtPersonelAdi.Text = reader["FirstName"].ToString();
+            //    txtPersonelSoyadi.Text = reader.GetString(1);
+            //}
+
+            //reader.Close();
+        }
+    }
+}
